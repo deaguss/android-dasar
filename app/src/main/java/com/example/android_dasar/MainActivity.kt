a8c38ab3b725bf1a53ddc940c58ebf52bbb8b0e8
deagus <dedeaskyt121@gmail.com>
2023-10-11 21:35:29 +0800

    add string-array di main activity

--- app/src/main/java/com/example/android_dasar/MainActivity.kt
+++ app/src/main/java/com/example/android_dasar/MainActivity.kt
@@ -12,12 +12,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var namedEditText: EditText
     private lateinit var sayHelloBtn: Button
     private lateinit var sayHelloText: TextView
+    private lateinit var textAllData: TextView
 
     private fun initComponents(){
         // manggil component berdasarkan id
         namedEditText = findViewById(R.id.nameEditText)
         sayHelloBtn = findViewById(R.id.sayHelloButton)
         sayHelloText = findViewById(R.id.sayHelloText)
+        textAllData = findViewById(R.id.allData)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +37,11 @@ class MainActivity : AppCompatActivity() {
             val name: String = namedEditText.text.toString()
 
             sayHelloText.text = resources.getString(R.string.say_hello_text, name)
+
+            // get all data resource string-array
+            val itemNames = resources.getStringArray(R.array.item_name)
+            val allDataText = itemNames.joinToString(", ") // Menggabungkan semua item dalam satu string dengan koma sebagai pemisah
+            textAllData.text = allDataText
 
         }
     }
