b075d6689fc4f95317b6df64dfe6312710a268fb
deagus <dedeaskyt121@gmail.com>
2023-10-11 20:28:54 +0800

    edit main activity

--- app/src/main/java/com/example/android_dasar/MainActivity.kt
+++ app/src/main/java/com/example/android_dasar/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.android_dasar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.hello_world)
+
+        val namedEditText: EditText = findViewById(R.id.nameEditText)
+        val sayHelloBtn: Button = findViewById(R.id.sayHelloButton)
+        val sayHelloText: TextView = findViewById(R.id.sayHelloText)
+
+        sayHelloText.text = "Hello world"
     }
 }
