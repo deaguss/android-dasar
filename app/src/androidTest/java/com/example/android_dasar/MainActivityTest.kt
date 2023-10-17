fd13d8bd65d62576a46f0c8c91b1957bacc420c7
deagus <dedeaskyt121@gmail.com>
2023-10-17 23:58:28 +0800

    masih ada bug di adnroid testing MainActivity tidak bisa dipanggil

--- app/src/androidTest/java/com/example/android_dasar/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/android_dasar/MainActivityTest.kt
@@ -2,24 +2,43 @@ package com.example.android_dasar
 
 import android.app.Activity
 import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class MainActivityTest {
-    lateinit var activityScenario: ActivityScenario<MainActivity>
 
-    @Before
-    fun setUp() {
-        activityScenario = ActivityScenario.launch(MainActivity::class.java)
+    // activity scenario testing
+    //    lateinit var activityScenario: ActivityScenario<MainActivity>
+    //
+    //    @Before
+    //    fun setUp() {
+    //        activityScenario = ActivityScenario.launch(MainActivity::class.java)
+    //    }
+    //
+    //    @After
+    //    fun tearDown() {
+    //        activityScenario.close()
+    //    }
+
+    // activity scenario blue testing lebih cocok untuk instrument test
+    @get:Rule
+    var activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testBlack(){
+        println("Testing....")
     }
 
-    @After
-    fun tearDown() {
-        activityScenario.close()
-    }
+
+    // Espresso berisikan banyak class yang bisa digunakan untuk mempermudah kita melakukan Instrumentation Test
+    // Karena terlalu banyak, jadi kita akan membahasnya sambil kelasnya berjalan, dan sambil kita praktekan
+    // Package Espresso ada di androidx.test.espresso
 }
 
 open class MainActivity : Activity() {}
